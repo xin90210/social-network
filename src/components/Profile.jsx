d37ea9ea4d0bd6196742968e90426c467d469d6b
unknown <xin90210@gmail.com>
2019-11-07 17:56:07 +0200

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return
+    <div className='content'>
+        <div><img src='http://we.com.mk/wp-content/uploads/2016/12/greece-islands.jpg' /></div>
+        <div className='ava'><img src='https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg' /></div>
+        <div>My posts
+      <div>New posts</div>
+            <div>
+                <div>
+                    Post1
+        </div>
+                <div> Post 2</div>
+            </div>
+        </div>
+        <div></div>
+    </div>
+</div >
+}
+
+export default Profile
+
