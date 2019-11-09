eecfc9098ff303407bd0530b24d2fd863de4f0df
unknown <xin90210@gmail.com>
2019-11-09 22:01:20 +0200

    header, navbar reworked

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
 const Profile = () => {
-    return
-    <div className='content'>
-        <div><img src='http://we.com.mk/wp-content/uploads/2016/12/greece-islands.jpg' /></div>
-        <div className='ava'><img src='https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg' /></div>
-        <div>My posts
-      <div>New posts</div>
+    return (
+        <div className='content'>
+            <div className='bg-photo'><img src='http://we.com.mk/wp-content/uploads/2016/12/greece-islands.jpg' /></div>
+            <div className='ava'><img src='https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg' /></div>
             <div>
+                My posts
+                <div>New posts</div>
                 <div>
-                    Post1
-        </div>
-                <div> Post 2</div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
             </div>
         </div>
-        <div></div>
-    </div>
+    )
 }
 
 export default Profile
